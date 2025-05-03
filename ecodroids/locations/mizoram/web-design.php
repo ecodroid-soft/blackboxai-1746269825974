a34ea942cf6e37ca124e48e5eb94a84d7aadef0f
ecodroid-soft <ecodroids@gmail.com>
2025-05-03 16:30:21 +0530

    Add ecodroids/locations/mizoram/web-design.php 1746270020522

---
+++ ecodroids/locations/mizoram/web-design.php
@@ -0,0 +1,15 @@
+<?php
+$location = "Mizoram";
+$service = "Web Design";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title><?php echo $service . " in " . $location; ?> - EcoDroids</title>
+</head>
+<body>
+    <h1><?php echo $service . " Services in " . $location; ?></h1>
+    <p>Welcome to our <?php echo $service; ?> services page for <?php echo $location; ?>.</p>
+</body>
+</html>
